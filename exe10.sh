e4e8a40ee6644635fc17bb05b2e2d12926ae3771
VENU <132057941+venu8555@users.noreply.github.com>
2023-04-28 16:19:30 +0530

    Add files via upload

---
+++ exe10.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#calculating the 5 subjects marks...
+echo "Enter the  5 subjects marks:" m1 m2 m3 m4 m5 marks enter below
+read m1 m2 m3 m4 m5
+
+sum=$(echo "$m1 + $m2 + $m3 + $m4 + $m5" | bc)
+avg=$(echo "scale=2; $sum / 5" | bc)
+
+echo "the total Average marks is : $avg"
+echo "very good...."
